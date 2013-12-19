16c939ba3e7fc0674e31bfa3eff3e94e953647ad
Alexander Vieth <alex@lagoa.com>
2013-12-18 19:20:07 -0500

    Introduce a Titan class
    The goal is to hide Java nonsense from the programmer as much as
    possible. With this commit, the programmer can make a new Titan with
    some config options (as plain JS objects) and then open(Sync) that thing
    to get a TitanGraphWrapper.

--- index.js
+++
@@ -1,7 +0,0 @@
-var TitanGremlin = require('./lib/titan-gremlin');
-// var TitanFactory = require('./lib/titan-factory');
-
-module.exports = {
-  Gremlin: TitanGremlin,
-  // Factory: TitanFactory
-};
